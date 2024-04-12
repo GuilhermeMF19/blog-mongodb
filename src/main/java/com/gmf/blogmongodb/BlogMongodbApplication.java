aa37c4a263037c5f89f2fa7adf628ec9b4c83f1f
Guilherme <guilherme_riba@hotmail.com>
2024-04-12 18:59:43 -0300

    Create project

---
+++ src/main/java/com/gmf/blogmongodb/BlogMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.gmf.blogmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmf/blogmongodb/BlogMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmf.blogmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
