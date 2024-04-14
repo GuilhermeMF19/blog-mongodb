5fb6d0bc5912a2d5410047c6efbc83b88c426e6d
Guilherme <guilherme_riba@hotmail.com>
2024-04-14 09:29:09 -0300

    Create Post service and resource

---
+++ src/main/java/com/gmf/blogmongodb/resources/PostResource.java
@@ -0,0 +1,60 @@
+package com.gmf.blogmongodb.resources;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.gmf.blogmongodb.domain.Post;
+import com.gmf.blogmongodb.services.PostService;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+	
+	@Autowired
+	private PostService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Post>> findAll() {
+		List<Post> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		Post obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody Post post) {
+		Post obj = service.insert(post);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> update(@RequestBody Post post, @PathVariable String id) {
+		service.update(post);
+		return ResponseEntity.noContent().build();
+	}
+}
---
+++ src/main/java/com/gmf/blogmongodb/services/PostService.java
@@ -0,0 +1,53 @@
+package com.gmf.blogmongodb.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.gmf.blogmongodb.domain.Post;
+import com.gmf.blogmongodb.repository.PostRepository;
+import com.gmf.blogmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public List<Post> findAll() {
+		return repo.findAll();
+	}
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public Post insert(Post obj) {
+		return repo.insert(obj);
+	}
+	
+	public void delete(String id) {
+		try {
+			repo.deleteById(id);			
+		} catch (EmptyResultDataAccessException e) {
+			throw new ObjectNotFoundException(id);
+		}
+	}
+	
+	public Post update(Post obj) {
+		Post newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(Post newObj, Post obj) {
+		newObj.setDate(obj.getDate());
+		newObj.setTitle(obj.getTitle());
+		newObj.setBody(obj.getBody());
+		newObj.setAuthor(obj.getAuthor());
+	}
+}
