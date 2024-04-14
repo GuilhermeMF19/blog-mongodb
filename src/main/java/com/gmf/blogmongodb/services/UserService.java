7fe35d36fa2f0a717e09f919e91e2d97dd366ec4
Guilherme <guilherme_riba@hotmail.com>
2024-04-13 23:38:40 -0300

    Create User delete

--- src/main/java/com/gmf/blogmongodb/services/UserService.java
+++ src/main/java/com/gmf/blogmongodb/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.gmf.blogmongodb.domain.User;
@@ -27,5 +28,13 @@ public class UserService {
 	
 	public User insert(User obj) {
 		return repo.insert(obj);
+	}
+	
+	public void delete(String id) {
+		try {
+			repo.deleteById(id);			
+		} catch (EmptyResultDataAccessException e) {
+			throw new ObjectNotFoundException(id);
+		}
 	}
 }
