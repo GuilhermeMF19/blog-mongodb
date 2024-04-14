b84118ff86878887e145e4a72f7de4bcca0308b6
Guilherme <guilherme_riba@hotmail.com>
2024-04-13 23:05:42 -0300

    Create User - findById and implements custom exception

--- src/main/java/com/gmf/blogmongodb/services/UserService.java
+++ src/main/java/com/gmf/blogmongodb/services/UserService.java
@@ -1,20 +1,27 @@
 package com.gmf.blogmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gmf.blogmongodb.domain.User;
 import com.gmf.blogmongodb.repository.UserRepository;
+import com.gmf.blogmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
