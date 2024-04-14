fd7d854ada623ca7b439a3618278a81a6e1736f7
Guilherme <guilherme_riba@hotmail.com>
2024-04-14 11:05:01 -0300

    Create query with many criteria

--- src/main/java/com/gmf/blogmongodb/resources/PostResource.java
+++ src/main/java/com/gmf/blogmongodb/resources/PostResource.java
@@ -1,6 +1,7 @@
 package com.gmf.blogmongodb.resources;
 
 import java.net.URI;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -63,6 +64,18 @@ public class PostResource {
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue = "") String text) {
 		text = URL.decodeParam(text);
 		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value="text", defaultValue = "") String text, 
+			@RequestParam(value="minDate", defaultValue = "") String minDate, 
+			@RequestParam(value="maxDate", defaultValue = "") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(text, new Date(0L));
+		Date max = URL.convertDate(text, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(list);
 	}
 }
