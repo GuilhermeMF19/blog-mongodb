3c4b91068ed1474d8920e2ecc20e27e1b8649806
Guilherme <guilherme_riba@hotmail.com>
2024-04-14 09:42:01 -0300

    Add comments to Post

--- src/main/java/com/gmf/blogmongodb/config/Instantiation.java
+++ src/main/java/com/gmf/blogmongodb/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.gmf.blogmongodb.domain.Post;
 import com.gmf.blogmongodb.domain.User;
 import com.gmf.blogmongodb.dto.AuthorDTO;
+import com.gmf.blogmongodb.dto.CommentDTO;
 import com.gmf.blogmongodb.repository.PostRepository;
 import com.gmf.blogmongodb.repository.UserRepository;
 
@@ -40,6 +41,13 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2024"), "Partiu viagem", "Vou viajar para São Paulo.", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2024"), "Bom dia", "Dia feliz.", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2024"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2024"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2024"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
